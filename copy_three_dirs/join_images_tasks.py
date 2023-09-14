eef5bf3aae4010cce906ddf388d89c9ca4a77f49
lexxai <lexxai@users.noreply.github.com>
2023-09-14 04:41:52 +0300

    v0.9.1  refactoring

---
+++ copy_three_dirs/join_images_tasks.py
@@ -0,0 +1,197 @@
+import asyncio
+import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from multiprocessing import cpu_count
+from pathlib import Path
+
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+
+try:
+    from copy_three_dirs.join_images_cv import join_images
+except ImportError:
+    from join_images_cv import join_images
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def pool_join_images_async_proc(
+    img1_list: list[Path],
+    img2_dict: dict,
+    output_path: Path,
+    verbose=False,
+    join_tasks=0,
+    join_similarity=False,
+) -> list[dict]:
+    max_processes = cpu_count() if not join_tasks else join_tasks
+    if max_processes > 61:
+        max_processes = 61
+    logger.info(f"Processes ({max_processes}) of Join files : {len(img1_list)}.")
+    args = {
+        "img1_path": None,
+        "img2_path": None,
+        "img_destination_path": output_path,
+        "verbose": verbose,
+        "join_similarity": join_similarity,
+    }
+    loop = asyncio.get_running_loop()
+    with ProcessPoolExecutor(max_processes) as pool:
+        futures = []
+        for img1_path in img1_list:
+            args["img1_path"] = img1_path
+            args["img2_path"] = img2_dict.get(img1_path.stem)
+            futures.append(loop.run_in_executor(pool, join_images, args))
+        with logging_redirect_tqdm():
+            pbar = tqdm(
+                asyncio.as_completed(futures),
+                total=len(futures),
+                desc=f"Join to {output_path.name: <9}",
+            )
+            results: list = [await t for t in pbar]
+    return results
+
+
+def pool_join_images_proc(
+    img1_list: list[Path],
+    img2_dict: dict,
+    output_path: Path,
+    verbose=False,
+    join_tasks=0,
+    join_similarity=False,
+) -> list[dict]:
+    max_processes = cpu_count() if not join_tasks else join_tasks
+    if max_processes > 61:
+        max_processes = 61
+    logger.info(f"Processes ({max_processes}) of Join files : {len(img1_list)}.")
+
+    # loop = asyncio.get_running_loop()
+    args = {
+        "img1_path": None,
+        "img2_path": None,
+        "img_destination_path": output_path,
+        "verbose": verbose,
+        "join_similarity": join_similarity,
+    }
+    with ProcessPoolExecutor(max_processes) as pool:
+        futures = []
+        for img1_path in img1_list:
+            args["img1_path"] = img1_path
+            args["img2_path"] = img2_dict.get(img1_path.stem)
+            futures.append(pool.submit(join_images, args))
+        with logging_redirect_tqdm():
+            pbar_future = tqdm(
+                concurrent.futures.as_completed(futures),
+                total=len(futures),
+                desc=f"Join to {output_path.name: <9}",
+            )
+            results: list = [future.result() for future in pbar_future]
+    return results
+
+
+def pool_join_images_thread(
+    img1_list: list[Path],
+    img2_dict: dict,
+    output_path: Path,
+    verbose=False,
+    join_tasks=0,
+    join_similarity=False,
+) -> list[dict]:
+    max_threads = cpu_count() * 4 + 2 if not join_tasks else join_tasks
+    logger.info(f"Threads ({max_threads}) of Join files : {len(img1_list)}.")
+    # loop = asyncio.get_running_loop()
+    args = {
+        "img1_path": None,
+        "img2_path": None,
+        "img_destination_path": output_path,
+        "verbose": verbose,
+        "join_similarity": join_similarity,
+    }
+    with ThreadPoolExecutor(max_threads) as pool:
+        futures = []
+        for img1_path in img1_list:
+            args["img1_path"] = img1_path
+            args["img2_path"] = img2_dict.get(img1_path.stem)
+            futures.append(pool.submit(join_images, args))
+
+        with logging_redirect_tqdm():
+            pbar_future = tqdm(
+                concurrent.futures.as_completed(futures),
+                total=len(futures),
+                desc=f"Join to {output_path.name: <9}",
+            )
+            results: list = [future.result() for future in pbar_future]
+    return results
+
+
+def join_images_one_core(
+    copy_list: list[Path],
+    found_dict: dict,
+    joined_path: Path,
+    verbose: bool = False,
+    join_tasks=0,
+    join_similarity=False,
+) -> list[dict]:
+    logger.info(f"One core process of Join files : {len(copy_list)}.")
+    results = []
+    args = {
+        "img1_path": None,
+        "img2_path": None,
+        "img_destination_path": joined_path,
+        "verbose": verbose,
+        "join_similarity": join_similarity,
+    }
+    with logging_redirect_tqdm():
+        for img1 in tqdm(
+            copy_list, total=len(copy_list), desc=f"Join to {joined_path.name: <9}"
+        ):
+            img2 = found_dict[img1.stem]
+            if img1.is_file() and img2.is_file():
+                args["img1_path"] = img1
+                args["img2_path"] = img2
+                # logger.info(f"join_images({img1}, {img2}, {joined_path})")
+                result = join_images(args)
+                results.append(result)
+    return results
+
+
+def join_images_future_core(
+    copy_list: list[Path],
+    found_dict: dict,
+    joined_path: Path,
+    verbose: bool = False,
+    join_tasks=0,
+    join_similarity=False,
+):
+    results = pool_join_images_proc(
+        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
+    )
+    return results
+
+
+def join_images_future_thread(
+    copy_list: list[Path],
+    found_dict: dict,
+    joined_path: Path,
+    verbose: bool = False,
+    join_tasks=0,
+    join_similarity=False,
+):
+    results = pool_join_images_thread(
+        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
+    )
+    return results
+
+
+async def join_images_future_core_async(
+    copy_list: list[Path],
+    found_dict: dict,
+    joined_path: Path,
+    verbose: bool = False,
+    join_tasks=0,
+    join_similarity=False,
+):
+    results = await pool_join_images_async_proc(
+        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
+    )
+    return results
--- copy_three_dirs/main.py
+++ copy_three_dirs/main.py
@@ -4,16 +4,16 @@ from shutil import copy
 import asyncio
 from multiprocessing import cpu_count
 from multiprocessing import freeze_support
-import concurrent.futures
 from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import ProcessPoolExecutor
 
 from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
+
 try:
     from copy_three_dirs.parse_args import app_arg
     from copy_three_dirs.export_data import export_to_csv, export_similarity_to_csv
+    import copy_three_dirs.join_images_tasks as ji_tasks
 
     from copy_three_dirs.join_images_cv import join_images
 
@@ -21,7 +21,7 @@ try:
 except ImportError:
     from parse_args import app_arg
     from export_data import export_to_csv, export_similarity_to_csv
-
+    import join_images_tasks as ji_tasks
     from join_images_cv import join_images
 
     # from join_images_pil import join_images
@@ -63,191 +63,10 @@ async def pool_copy_files(
     return error_files
 
 
-async def pool_join_images_async_proc(
-    img1_list: list[Path],
-    img2_dict: dict,
-    output_path: Path,
-    verbose=False,
-    join_tasks=0,
-    join_similarity=False,
-) -> list[dict]:
-    max_processes = cpu_count() if not join_tasks else join_tasks
-    if max_processes > 61:
-        max_processes = 61
-    logger.info(f"Processes ({max_processes}) of Join files : {len(img1_list)}.")
-    args = {
-        "img1_path": None,
-        "img2_path": None,
-        "img_destination_path": output_path,
-        "verbose": verbose,
-        "join_similarity": join_similarity,
-    }
-    loop = asyncio.get_running_loop()
-    with ProcessPoolExecutor(max_processes) as pool:
-        futures = []
-        for img1_path in img1_list:
-            args["img1_path"] = img1_path
-            args["img2_path"] = img2_dict.get(img1_path.stem)
-            futures.append(loop.run_in_executor(pool, join_images, args))
-        with logging_redirect_tqdm():
-            pbar = tqdm(
-                asyncio.as_completed(futures),
-                total=len(futures),
-                desc=f"Join to {output_path.name: <9}",
-            )
-            results: list = [await t for t in pbar]
-    return results
-
-
-def pool_join_images_proc(
-    img1_list: list[Path],
-    img2_dict: dict,
-    output_path: Path,
-    verbose=False,
-    join_tasks=0,
-    join_similarity=False,
-) -> list[dict]:
-    max_processes = cpu_count() if not join_tasks else join_tasks
-    if max_processes > 61:
-        max_processes = 61
-    logger.info(f"Processes ({max_processes}) of Join files : {len(img1_list)}.")
-
-    # loop = asyncio.get_running_loop()
-    args = {
-        "img1_path": None,
-        "img2_path": None,
-        "img_destination_path": output_path,
-        "verbose": verbose,
-        "join_similarity": join_similarity,
-    }
-    with ProcessPoolExecutor(max_processes) as pool:
-        futures = []
-        for img1_path in img1_list:
-            args["img1_path"] = img1_path
-            args["img2_path"] = img2_dict.get(img1_path.stem)
-            futures.append(pool.submit(join_images, args))
-        with logging_redirect_tqdm():
-            pbar_future = tqdm(
-                concurrent.futures.as_completed(futures),
-                total=len(futures),
-                desc=f"Join to {output_path.name: <9}",
-            )
-            results: list = [future.result() for future in pbar_future]
-    return results
-
-
-def pool_join_images_thread(
-    img1_list: list[Path],
-    img2_dict: dict,
-    output_path: Path,
-    verbose=False,
-    join_tasks=0,
-    join_similarity=False,
-) -> list[dict]:
-    max_threads = cpu_count() * 4 + 2 if not join_tasks else join_tasks
-    logger.info(f"Threads ({max_threads}) of Join files : {len(img1_list)}.")
-    # loop = asyncio.get_running_loop()
-    args = {
-        "img1_path": None,
-        "img2_path": None,
-        "img_destination_path": output_path,
-        "verbose": verbose,
-        "join_similarity": join_similarity,
-    }
-    with ThreadPoolExecutor(max_threads) as pool:
-        futures = []
-        for img1_path in img1_list:
-            args["img1_path"] = img1_path
-            args["img2_path"] = img2_dict.get(img1_path.stem)
-            futures.append(pool.submit(join_images, args))
-
-        with logging_redirect_tqdm():
-            pbar_future = tqdm(
-                concurrent.futures.as_completed(futures),
-                total=len(futures),
-                desc=f"Join to {output_path.name: <9}",
-            )
-            results: list = [future.result() for future in pbar_future]
-    return results
-
-
-def join_images_one_core(
-    copy_list: list[Path],
-    found_dict: dict,
-    joined_path: Path,
-    verbose: bool = False,
-    join_tasks=0,
-    join_similarity=False,
-) -> list[dict]:
-    logger.info(f"One core process of Join files : {len(copy_list)}.")
-    results = []
-    args = {
-        "img1_path": None,
-        "img2_path": None,
-        "img_destination_path": joined_path,
-        "verbose": verbose,
-        "join_similarity": join_similarity,
-    }
-    with logging_redirect_tqdm():
-        for img1 in tqdm(
-            copy_list, total=len(copy_list), desc=f"Join to {joined_path.name: <9}"
-        ):
-            img2 = found_dict[img1.stem]
-            if img1.is_file() and img2.is_file():
-                args["img1_path"] = img1
-                args["img2_path"] = img2
-                # logger.info(f"join_images({img1}, {img2}, {joined_path})")
-                result = join_images(args)
-                results.append(result)
-    return results
-
-
-def join_images_future_core(
-    copy_list: list[Path],
-    found_dict: dict,
-    joined_path: Path,
-    verbose: bool = False,
-    join_tasks=0,
-    join_similarity=False,
-):
-    results = pool_join_images_proc(
-        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
-    )
-    return results
-
-
-def join_images_future_thread(
-    copy_list: list[Path],
-    found_dict: dict,
-    joined_path: Path,
-    verbose: bool = False,
-    join_tasks=0,
-    join_similarity=False,
-):
-    results = pool_join_images_thread(
-        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
-    )
-    return results
-
-
-async def join_images_future_core_async(
-    copy_list: list[Path],
-    found_dict: dict,
-    joined_path: Path,
-    verbose: bool = False,
-    join_tasks=0,
-    join_similarity=False,
-):
-    results = await pool_join_images_async_proc(
-        copy_list, found_dict, joined_path, verbose, join_tasks, join_similarity
-    )
-    return results
-
-
 async def main_async(args):
     # print(args)
+    # parse parameters of cli
     work_path = Path(args["work"])
-
     input1_path = Path(args["input1"])
     input2_path = Path(args["input2"])
     output_path = Path(args["output"])
@@ -256,6 +75,7 @@ async def main_async(args):
     found_path = Path(args["found"])
     joined_path = Path(args["joined"])
     csv_path = Path(args["csv"])
+    # add work prefix to path is path is not absolute
     if not input1_path.is_absolute():
         input1_path = work_path.joinpath(input1_path)
     if not input2_path.is_absolute():
@@ -278,18 +98,17 @@ async def main_async(args):
     join_tasks = args.get("join_tasks")
     join_similarity = args.get("join_similarity")
 
+    # find files in source directories and build dict with keys as stem of filename
     input1_files = {i.stem: i for i in input1_path.glob("*.*")}
-    # print(input1_files)
-    # input2_files = list(input2_path.glob("*.*"))
     input2_files = {i.stem: i for i in input2_path.glob("*.*")}
-
+    # build sets of filenames by their stem for search later difference
     input1_files_set = set(input1_files.keys())
     input2_files_set = set(input2_files.keys())
 
     output_path.mkdir(exist_ok=True, parents=True)
     copy_list = []
     found_dict = {}
-    # not_found1_list = []
+    # build list of files for copy
     for files2_stem, files2 in input2_files.items():
         file_src = input1_files.get(files2_stem)
         if file_src:
@@ -297,20 +116,13 @@ async def main_async(args):
             found_dict[files2_stem] = files2
 
     found_list: list[Path] = list(found_dict.values())
-
-    # not_found1_dict = input1_files.copy()
-    # for copy_item in copy_list:
-    #     item = not_found1_dict.get(copy_item.stem)
-    #     if item:
-    #         del not_found1_dict[copy_item.stem]
-    # not_found1_list: list[Path] = list(not_found1_dict.values())
-
+    # find difference input1 from input2
     not_found1_difference = input1_files_set.difference(input2_files_set)
     not_found1_list: list[Path] = [input1_files[item] for item in not_found1_difference]
-
+    # find difference input2 from input1
     not_found2_difference = input2_files_set.difference(input1_files_set)
     not_found2_list: list[Path] = [input2_files[item] for item in not_found2_difference]
-
+    # check if need only joint than skip copy tasks
     if not to_join_only:
         print(
             f"The Input1 folder '{input1_path.name}' consist of files: {len(input1_files)}"
@@ -319,18 +131,19 @@ async def main_async(args):
             f"The Input2 folder '{input2_path.name}' consist of files: {len(input2_files)}"
         )
         logger.info(f"Copy only common files by name to '{output_path}' folder")
+        # async copy files from copy_list
         if copy_list:
             error_files = await pool_copy_files(copy_list, output_path)
             if error_files:
                 print(f"\nError copy files ({len(error_files)}): {error_files}")
-
+        # async copy files from found_list
         if found_list:
             found_path.mkdir(exist_ok=True, parents=True)
             logger.info(f"Copy found files to '{found_path}' folder")
             error_files = await pool_copy_files(found_list, found_path)
             if error_files:
                 print(f"\nError copy files ({len(error_files)}): {error_files}")
-
+        # async copy files from not_found1_list
         if not_found1_list:
             notfound1_path.mkdir(exist_ok=True, parents=True)
             logger.info(f"Copy notfound files to '{notfound1_path}' folder")
@@ -338,7 +151,7 @@ async def main_async(args):
             if error_files:
                 print(f"\nError copy files ({len(error_files)}): {error_files}")
             export_to_csv(not_found1_list, csv_path.joinpath("not_found1.csv"))
-
+        # async copy files from not_found2_list
         if not_found2_list:
             notfound2_path.mkdir(exist_ok=True, parents=True)
             logger.info(f"Copy notfound files to '{notfound2_path}' folder")
@@ -346,21 +159,15 @@ async def main_async(args):
             if error_files:
                 print(f"\nError copy files ({len(error_files)}): {error_files}")
             export_to_csv(not_found2_list, csv_path.joinpath("not_found2.csv"))
-    # to join
+    # tasks of join images from directories copy_list and found_list and check similarity of them
     if (to_join or to_join_only) and copy_list and found_list:
-        # joined_path.mkdir(exist_ok=True, parents=True)
-        # error_files = await pool_join_images(
-        #     copy_list, found_dict, joined_path, verbose=args.get("verbose")
-        # )
-        # if error_files:
-        #     print(f"\nError join files ({len(error_files)}): {error_files}")
-
         joined_path.mkdir(exist_ok=True, parents=True)
         results = []
+        # check what method of CPU bounding task will to use, by default will to used method 'future_thread'
         match join_mode:
             case "one_core":
                 # one_core
-                results = join_images_one_core(
+                results = ji_tasks.join_images_one_core(
                     copy_list,
                     found_dict,
                     joined_path,
@@ -370,7 +177,7 @@ async def main_async(args):
                 )
             case "future_core":
                 # future_core
-                results = join_images_future_core(
+                results = ji_tasks.join_images_future_core(
                     copy_list,
                     found_dict,
                     joined_path,
@@ -380,7 +187,7 @@ async def main_async(args):
                 )
             case "future_thread":
                 # future_thread
-                results = join_images_future_thread(
+                results = ji_tasks.join_images_future_thread(
                     copy_list,
                     found_dict,
                     joined_path,
@@ -390,7 +197,7 @@ async def main_async(args):
                 )
             case "future_core_async":
                 # future_thread
-                results = await join_images_future_core_async(
+                results = await ji_tasks.join_images_future_core_async(
                     copy_list,
                     found_dict,
                     joined_path,
@@ -403,7 +210,9 @@ async def main_async(args):
 
         # after join tasks
         if join_similarity:
+            # export similarity data for file CSV
             export_similarity_to_csv(results, csv_path.joinpath("join_similarity.csv"))
+        # filter from results only error_files list
         error_files = list(filter(lambda t: t.get("error") is not None, results))
         if error_files:
             print(f"\nError join files ({len(error_files)}): {error_files}")
@@ -423,7 +232,11 @@ def main():
         format="%(asctime)s [ %(threadName)s ]  %(message)s",
     )
     logger = logging.getLogger(__name__)
-    asyncio.run(main_async(args))
+    # async io tasks
+    try:
+        asyncio.run(main_async(args))
+    except KeyboardInterrupt as e:
+        logger.info("Keyboard Interrupt")
 
 
 if __name__ == "__main__":
